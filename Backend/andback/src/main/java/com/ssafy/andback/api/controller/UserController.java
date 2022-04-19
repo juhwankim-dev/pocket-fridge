bee65256b4f676619de8fbe345d422634415e31d
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-04-19 12:29:07 +0900

    S06P31D206-34 [be] feat: :sparkles: 유저 관련 파일 생성

---
+++ Backend/andback/src/main/java/com/ssafy/andback/api/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.ssafy.andback.api.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+*
+* UserController
+* 유저와 관련된 api를 모은 Controller
+*
+* @author 김다은
+* @version 1.0.0
+* 생성일 2022-04-19
+* 마지막 수정일 2022-04-19
+**/
+
+@Api(value = "유저 API", tags = {"User"})
+@RestController
+@RequestMapping("/user")
+public class UserController {
+}
---
+++ Backend/andback/src/main/java/com/ssafy/andback/api/service/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.ssafy.andback.api.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+*
+* UserServiceImpl
+* UserService 구체화
+*
+* @author 김다은
+* @version 1.0.0
+* 생성일 2022-04-19
+* 마지막 수정일 2022-04-19
+**/
+
+@Service("userService")
+public class UserServiceImpl implements UserService{
+}
