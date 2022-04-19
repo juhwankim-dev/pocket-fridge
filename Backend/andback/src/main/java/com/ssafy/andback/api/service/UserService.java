a594dc3d0972d29e6c7974fd219a972ad007a3da
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-04-19 12:29:07 +0900

    S06P31D206-34 [be] feat: :sparkles: 유저 관련 파일 생성

---
+++ Backend/andback/src/main/java/com/ssafy/andback/api/service/UserService.java
@@ -0,0 +1,14 @@
+package com.ssafy.andback.api.service;
+
+/**
+*
+* UserService
+* UserController 에서 받은 데이터를 처리 (인터페이스)
+*
+* @author 김다은
+* @version 1.0.0
+* 생성일 2022-04-19
+* 마지막 수정일 2022-04-19
+**/
+public interface UserService {
+}
