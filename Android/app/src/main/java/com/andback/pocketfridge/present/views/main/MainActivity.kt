ca76e3a9a96518126c1e17de47f0bc89eb297713
김민수 <ms001118@naver.com>
2022-04-20 13:38:20 +0900

    S06P31D206-152 [mo] fix: BottomNaviagtion 세팅 함수 추출

--- Android/app/src/main/java/com/andback/pocketfridge/present/views/main/MainActivity.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.andback.pocketfridge.present.views.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -10,9 +12,13 @@ import com.andback.pocketfridge.databinding.ActivityMainBinding
 import com.andback.pocketfridge.present.config.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private val TAG = "MainActivity_debuk"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setBottomNav()
+    }
 
+    private fun setBottomNav() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
         val navController = navHostFragment.findNavController()
         binding.bnvMain.setupWithNavController(navController)
