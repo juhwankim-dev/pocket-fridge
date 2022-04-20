e418012f255d0b4fa69a8130d428514524a001ff
김민수 <ms001118@naver.com>
2022-04-20 10:35:53 +0900

    S06P31D206-152 [mo] feat: MainActivity BaseActivity 상속

--- Android/app/src/main/java/com/andback/pocketfridge/present/views/main/MainActivity.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/MainActivity.kt
@@ -3,10 +3,12 @@ package com.andback.pocketfridge.present.views.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.andback.pocketfridge.R
+import com.andback.pocketfridge.databinding.ActivityMainBinding
+import com.andback.pocketfridge.present.config.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
     }
 }
