e083b09849a44ee31d3348bd492ad799fc940ab1
김민수 <ms001118@naver.com>
2022-04-20 11:27:47 +0900

    S06P31D206-152 [mo] feat: BottomNavigationView 세팅

--- Android/app/src/main/java/com/andback/pocketfridge/present/views/main/MainActivity.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/MainActivity.kt
@@ -2,6 +2,9 @@ package com.andback.pocketfridge.present.views.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.andback.pocketfridge.R
 import com.andback.pocketfridge.databinding.ActivityMainBinding
 import com.andback.pocketfridge.present.config.BaseActivity
@@ -10,5 +13,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        binding.bnvMain.setupWithNavController(navController)
     }
 }
