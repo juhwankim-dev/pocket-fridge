26f584fbd8841ef7f654e22993734e899ca80c5e
Juhwan Kim <juhwan.dev@gmail.com>
2022-04-22 11:04:54 +0900

    S06P31D206-31 [mo] feat: :apple: Step1, Step2 페이지 (= 회원가입) 페이지 추가

---
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/user/signup/StepTwoFragment.kt
@@ -0,0 +1,9 @@
+package com.andback.pocketfridge.present.views.user.signup
+
+import com.andback.pocketfridge.R
+import com.andback.pocketfridge.databinding.FragmentStepTwoBinding
+import com.andback.pocketfridge.present.config.BaseFragment
+
+class StepTwoFragment : BaseFragment<FragmentStepTwoBinding>(R.layout.fragment_step_two) {
+
+}
