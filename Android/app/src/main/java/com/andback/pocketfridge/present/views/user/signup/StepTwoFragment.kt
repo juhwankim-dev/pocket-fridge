67cf68ffc9249a476599c8115437683861e0b477
Juhwan Kim <juhwan.dev@gmail.com>
2022-04-23 22:16:35 +0900

    S06P31D206-31 [mo] feat: :sparkles: 페이지 전환 구현

--- Android/app/src/main/java/com/andback/pocketfridge/present/views/user/signup/StepTwoFragment.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/user/signup/StepTwoFragment.kt
@@ -1,9 +1,33 @@
 package com.andback.pocketfridge.present.views.user.signup
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
 import com.andback.pocketfridge.R
 import com.andback.pocketfridge.databinding.FragmentStepTwoBinding
 import com.andback.pocketfridge.present.config.BaseFragment
+import com.andback.pocketfridge.present.utils.PageSet
+import com.andback.pocketfridge.present.views.user.UserActivity
+import com.andback.pocketfridge.present.views.user.UserViewModel
 
 class StepTwoFragment : BaseFragment<FragmentStepTwoBinding>(R.layout.fragment_step_two) {
+    private val viewModel: UserViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViewModelCallback()
+    }
+
+    private fun initViewModelCallback() {
+        with(viewModel) {
+            pageNumber.observe(viewLifecycleOwner) {
+                when (pageNumber.value) {
+                    PageSet.STEP_ONE -> (context as UserActivity).onChangeFragement(PageSet.STEP_ONE)
+                    PageSet.STEP_TWO -> (context as UserActivity).onChangeFragement(PageSet.STEP_TWO)
+                    PageSet.LOGIN -> (context as UserActivity).onChangeFragement(PageSet.LOGIN)
+                }
+            }
+        }
+    }
 }
