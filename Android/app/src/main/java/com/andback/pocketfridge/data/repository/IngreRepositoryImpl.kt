90dc813ccec70d86c18caaa05850cb6f3833fd49
김민수 <ms001118@naver.com>
2022-04-23 17:20:55 +0900

    S06P31D206-162 [mo] feat: :sparkles: IngreRepositoryImpl 구현

--- Android/app/src/main/java/com/andback/pocketfridge/data/repository/IngreRepositoryImpl.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/data/repository/IngreRepositoryImpl.kt
@@ -1,4 +1,14 @@
-package com.andback.pocketfridge.data.repository.ingredient
+package com.andback.pocketfridge.data.repository
 
-class IngreRepositoryImpl {
+import com.andback.pocketfridge.data.model.IngreEntityForUpload
+import com.andback.pocketfridge.data.model.TempEntity
+import com.andback.pocketfridge.data.repository.ingredient.IngreRemoteDataSourceImpl
+import com.andback.pocketfridge.domain.repository.IngreRepository
+import io.reactivex.Single
+import javax.inject.Inject
+
+class IngreRepositoryImpl @Inject constructor(val ingreRemoteDataSource: IngreRemoteDataSourceImpl): IngreRepository {
+    override fun uploadIngre(ingreEntityForUpload: IngreEntityForUpload): Single<TempEntity> {
+        return ingreRemoteDataSource.uploadIngre(ingreEntityForUpload)
+    }
 }
