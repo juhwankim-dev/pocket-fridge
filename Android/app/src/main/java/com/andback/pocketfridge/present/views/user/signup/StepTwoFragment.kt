2c5e2dcbca7b4c7e3c02d790e74e6c109ffed3c4
Juhwan Kim <juhwan.dev@gmail.com>
2022-04-25 01:11:47 +0900

    S06P31D206-25 [mo] feat: :sparkles: 유효성 검사 구현 및 viewModel 초기화 방식 변경

--- Android/app/src/main/java/com/andback/pocketfridge/present/views/user/signup/StepTwoFragment.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/user/signup/StepTwoFragment.kt
@@ -3,6 +3,7 @@ package com.andback.pocketfridge.present.views.user.signup
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import com.andback.pocketfridge.R
 import com.andback.pocketfridge.databinding.FragmentStepTwoBinding
 import com.andback.pocketfridge.present.config.BaseFragment
@@ -10,18 +11,20 @@ import com.andback.pocketfridge.present.utils.PageSet
 import com.andback.pocketfridge.present.views.user.UserActivity
 import com.andback.pocketfridge.present.views.user.UserViewModel
 
-private const val TAG = "StepTwoFragment_juhwan"
-
 class StepTwoFragment : BaseFragment<FragmentStepTwoBinding>(R.layout.fragment_step_two) {
-    private val viewModel: UserViewModel by viewModels()
+    lateinit var viewModel: UserViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initViewModelCallback()
+        initViewModel()
+        initEvent()
     }
 
-    private fun initViewModelCallback() {
+    private fun initViewModel() {
+        viewModel = ViewModelProvider(requireActivity()).get(UserViewModel::class.java)
+        binding.vm = viewModel
+
         with(viewModel) {
             pageNumber.observe(viewLifecycleOwner) {
                 when (pageNumber.value) {
@@ -31,5 +34,9 @@ class StepTwoFragment : BaseFragment<FragmentStepTwoBinding>(R.layout.fragment_s
                 }
             }
         }
+    }
+
+    private fun initEvent() {
+
     }
 }
