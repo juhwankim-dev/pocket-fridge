d62b5936d7aeb3bcbd37829b67713a1d462b2355
Juhwan Kim <juhwan.dev@gmail.com>
2022-04-25 01:12:23 +0900

    S06P31D206-25 [mo] feat: :sparkles: @HiltAndroidApp 어노테이션 추가

--- Android/app/src/main/java/com/andback/pocketfridge/present/config/ApplicationClass.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/config/ApplicationClass.kt
@@ -1,10 +1,12 @@
 package com.andback.pocketfridge.present.config
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
+@HiltAndroidApp
 class ApplicationClass : Application() {
     private val baseUrl = "https://hoonycode.loca.lt:443/"
 
