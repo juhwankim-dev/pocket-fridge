ac00ffadbe4b3d9e3aff84c9b2e7d42e8c6414a3
김민수 <ms001118@naver.com>
2022-04-24 17:30:38 +0900

    S06P31D206-163 [mo] feat: ingreService 추가

--- Android/app/src/main/java/com/andback/pocketfridge/present/utils/RetrofitUtil.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/utils/RetrofitUtil.kt
@@ -1,10 +1,12 @@
 package com.andback.pocketfridge.present.utils
 
+import com.andback.pocketfridge.data.api.IngreApi
 import com.andback.pocketfridge.data.api.UserApi
 import com.andback.pocketfridge.present.config.ApplicationClass
 
 class RetrofitUtil {
     companion object {
         val userService = ApplicationClass.retrofit.create(UserApi::class.java)
+        val ingreService = ApplicationClass.retrofit.create(IngreApi::class.java)
     }
 }
