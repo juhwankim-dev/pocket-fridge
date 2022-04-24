b93a8f0840d4a1a2aa835164d54792aba4d36f5b
김민수 <ms001118@naver.com>
2022-04-24 17:28:53 +0900

    S06P31D206-163 [mo] feat: :sparkles: IngreRepository 제공방법 추가

--- Android/app/src/main/java/com/andback/pocketfridge/data/di/RepositoryModule.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/data/di/RepositoryModule.kt
@@ -1,7 +1,10 @@
 package com.andback.pocketfridge.data.di
 
+import com.andback.pocketfridge.data.repository.IngreRepositoryImpl
 import com.andback.pocketfridge.data.repository.UserRepositoryImpl
+import com.andback.pocketfridge.data.repository.ingredient.IngreRemoteDataSource
 import com.andback.pocketfridge.data.repository.user.UserRemoteDataSource
+import com.andback.pocketfridge.domain.repository.IngreRepository
 import com.andback.pocketfridge.domain.repository.UserRepository
 import dagger.Module
 import dagger.Provides
@@ -16,5 +19,11 @@ class RepositoryModule {
     @Singleton
     fun provideSignUpRepository(userRemoteDataSource: UserRemoteDataSource): UserRepository {
         return UserRepositoryImpl(userRemoteDataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideIngreRepository(ingreRemoteDataSource: IngreRemoteDataSource): IngreRepository {
+        return IngreRepositoryImpl(ingreRemoteDataSource)
     }
 }
