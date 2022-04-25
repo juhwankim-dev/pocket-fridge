f9d4966966590eaab56b2796e6e0761b749cc727
김민수 <ms001118@naver.com>
2022-04-25 16:24:10 +0900

    S06P31D206-167 [mo] fix: :bug: Single에서 Observable로 변경

--- Android/app/src/main/java/com/andback/pocketfridge/data/api/IngreApi.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/data/api/IngreApi.kt
@@ -2,11 +2,12 @@ package com.andback.pocketfridge.data.api
 
 import com.andback.pocketfridge.data.model.BaseResponse
 import com.andback.pocketfridge.data.model.IngreEntityForUpload
+import io.reactivex.Observable
 import io.reactivex.Single
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface IngreApi {
     @POST("foodIngredient/insert")
-    fun uploadIngre(@Body ingredient: IngreEntityForUpload): Single<BaseResponse<Any>>
+    fun uploadIngre(@Body ingredient: IngreEntityForUpload): Observable<BaseResponse<Any>>
 }
--- Android/app/src/main/java/com/andback/pocketfridge/data/repository/ingredient/IngreRemoteDataSourceImpl.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/data/repository/ingredient/IngreRemoteDataSourceImpl.kt
@@ -3,11 +3,12 @@ package com.andback.pocketfridge.data.repository.ingredient
 import com.andback.pocketfridge.data.api.IngreApi
 import com.andback.pocketfridge.data.model.BaseResponse
 import com.andback.pocketfridge.data.model.IngreEntityForUpload
+import io.reactivex.Observable
 import io.reactivex.Single
 import javax.inject.Inject
 
 class IngreRemoteDataSourceImpl @Inject constructor(val ingreApi: IngreApi): IngreRemoteDataSource {
-    override fun uploadIngre(ingreEntityForUpload: IngreEntityForUpload): Single<BaseResponse<Any>> {
+    override fun uploadIngre(ingreEntityForUpload: IngreEntityForUpload): Observable<BaseResponse<Any>> {
         return ingreApi.uploadIngre(ingreEntityForUpload)
     }
 }
