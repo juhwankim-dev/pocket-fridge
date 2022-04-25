92a821ca92503f1a7a66e19d18a25710baa00c1a
hoonyCode <rladudgns456@naver.com>
2022-04-25 13:43:10 +0900

    S06P31D206-164 [be] feat: :sparkles: 냉장고 등록 기능 추가

--- Backend/andback/src/main/java/com/ssafy/andback/api/controller/RefrigeratorController.java
+++ Backend/andback/src/main/java/com/ssafy/andback/api/controller/RefrigeratorController.java
@@ -1,5 +1,6 @@
 package com.ssafy.andback.api.controller;
 
+import com.ssafy.andback.api.dto.request.InsertRefrigeratorReqDto;
 import com.ssafy.andback.api.dto.response.ListResponseDto;
 import com.ssafy.andback.api.dto.response.RefrigeratorResDto;
 import com.ssafy.andback.api.dto.response.BaseResponseDto;
@@ -9,9 +10,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,12 +23,24 @@ public class RefrigeratorController {
 
     @ApiOperation(value = "냉장고 조회", notes = "사용자의 냉장고 리스트를 보여준다")
     @GetMapping("/{userEmail}")
-    public ResponseEntity<ListResponseDto<RefrigeratorResDto>> insertFoodIngredient(@ApiParam(value = "유저 이메일", required = true, example = "test@google") @PathVariable String userEmail) {
+    public ResponseEntity<ListResponseDto<RefrigeratorResDto>> findRefrigeratorList(@ApiParam(value = "유저 이메일", required = true, example = "test@google") @PathVariable String userEmail) {
 
         List<RefrigeratorResDto> response = refrigeratorService.findAllRefrigeratorByUser(userEmail);
 
         return ResponseEntity.ok(new ListResponseDto<RefrigeratorResDto>(200, "success", response));
     }
 
+    @ApiOperation(value = "냉장고 생성", notes = "사용자의 새로운 냉장고를 생성한다")
+    @PostMapping
+    public ResponseEntity<BaseResponseDto> insertRefrigerator(InsertRefrigeratorReqDto reqDto) {
+
+        String response = refrigeratorService.insertRefrigerator(reqDto);
+
+        if (response.equals("fail")){
+            return ResponseEntity.status(401).body(BaseResponseDto.of(401, "실패"));
+        }
+
+        return ResponseEntity.ok(BaseResponseDto.of(200, "success"));
+    }
 
 }
