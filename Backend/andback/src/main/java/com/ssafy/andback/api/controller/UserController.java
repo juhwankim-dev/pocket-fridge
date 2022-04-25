72a4a5f6c051da0cc7db1f3e3edec7107cd165b0
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-04-25 11:56:42 +0900

    S06P31D206-33 [be] feat : :sparkles: 유효성 검사 기능 구현

--- Backend/andback/src/main/java/com/ssafy/andback/api/controller/UserController.java
+++ Backend/andback/src/main/java/com/ssafy/andback/api/controller/UserController.java
@@ -1,28 +1,29 @@
 package com.ssafy.andback.api.controller;
 
-import io.swagger.annotations.Api;
+import io.swagger.annotations.*;
 import com.ssafy.andback.api.dto.UserDto;
 import com.ssafy.andback.api.dto.response.BaseResponseDto;
 import com.ssafy.andback.api.service.UserService;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.validation.Valid;
 
 /**
-*
-* UserController
-* 유저와 관련된 api를 모은 Controller
-*
-* @author 김다은
-* @version 1.0.0
-* 생성일 2022-04-19
-* 마지막 수정일 2022-04-19
-**/
+ * UserController
+ * 유저와 관련된 api를 모은 Controller
+ *
+ * @author 김다은
+ * @version 1.0.0
+ * 생성일 2022-04-19
+ * 마지막 수정일 2022-04-19
+ **/
 
 @Api(value = "유저 API", tags = {"User"})
 @RestController
@@ -34,41 +35,14 @@ public class UserController {
 
     // Swagger UI : API 설명 추가
     @ApiOperation(value = "회원가입", notes = "유저 정보를 받아 DB에 저장한다.")
-    @ApiImplicitParams(
-            {
-                    @ApiImplicitParam(
-                            name = "userEmail",
-                            value = "유저 이메일",
-                            required = true
-                    ),
-                    @ApiImplicitParam(
-                            name = "userName",
-                            value = "유저 이름",
-                            required = true
-                    ),
-                    @ApiImplicitParam(
-                            name = "userNickname",
-                            value = "유저 닉네임",
-                            required = true
-                    ),
-                    @ApiImplicitParam(
-                            name = "userPassword",
-                            value = "유저 비밀번호",
-                            required = true
-                    ),
-                    @ApiImplicitParam(
-                            name = "userPicture",
-                            value = "유저 사진 (null 가능)"
-                    )
-            }
-    )
+    @ApiParam(name = "errors", hidden = true)
     @PostMapping("/signup")
-    public ResponseEntity<BaseResponseDto> signUp(UserDto userDto){
+    public ResponseEntity<BaseResponseDto> signUp(@Valid UserDto userDto, @ApiIgnore Errors errors) {
         String result = userService.insertUser(userDto);
-
-        if(result.equals("fail")){
+        if (errors.hasErrors() || result.equals("fail")) {
             return ResponseEntity.status(401).body(BaseResponseDto.of(401, "회원가입 실패"));
         }
+
         return ResponseEntity.ok(BaseResponseDto.of(200, "회원가입 성공"));
     }
 }
