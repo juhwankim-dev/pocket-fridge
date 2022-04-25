5b2e960ac274eac15acf414fcfe89426484f3f7d
김민수 <ms001118@naver.com>
2022-04-25 13:00:44 +0900

    S06P31D206-160 [mo] feat: :sparkles: 식재료 등록 Usecase 추가

---
+++ Android/app/src/main/java/com/andback/pocketfridge/domain/usecase/UploadIngreUseCase.kt
@@ -0,0 +1,34 @@
+package com.andback.pocketfridge.domain.usecase
+
+import com.andback.pocketfridge.data.model.BaseResponse
+import com.andback.pocketfridge.data.model.IngreEntityForUpload
+import com.andback.pocketfridge.domain.model.Ingredient
+import com.andback.pocketfridge.domain.repository.IngreRepository
+import com.andback.pocketfridge.present.utils.IngreChecker
+import io.reactivex.Observable
+import java.lang.Exception
+import javax.inject.Inject
+
+class UploadIngreUseCase @Inject constructor(
+    val ingreRepository: IngreRepository
+) {
+    fun uploadIngre(ingredient: Ingredient): Observable<BaseResponse<Any>> {
+        return checkValidationThenUpload(ingredient)
+    }
+
+    private fun checkValidationThenUpload(ingredient: Ingredient): Observable<BaseResponse<Any>> {
+        return try {
+            IngreChecker.check(ingredient)
+            val ingredientEntity = IngreEntityForUpload(ingredient.name, ingredient.expiryDate, ingredient.purchasedDate, ingredient.fridgeId, category = ingredient.category, storage = ingredient.storage.value)
+            ingreRepository.uploadIngre(ingreEntityForUpload = ingredientEntity)
+        } catch (e: Exception) {
+            makeValidationErrorObservable(e)
+        }
+    }
+
+    private fun makeValidationErrorObservable(e: Exception): Observable<BaseResponse<Any>> {
+        return Observable.create { emitter ->
+            emitter.onError(e)
+        }
+    }
+}
