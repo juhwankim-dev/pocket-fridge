aa30ca3c565451b43708b57a433f739bfa5cd745
hoonyCode <rladudgns456@naver.com>
2022-04-25 09:47:10 +0900

    S06P31D206-164 [be] style: :art: Entity 주석 추가

--- Backend/andback/src/main/java/com/ssafy/andback/core/domain/Refrigerator.java
+++ Backend/andback/src/main/java/com/ssafy/andback/core/domain/Refrigerator.java
@@ -24,6 +24,17 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 @Setter
+
+/**
+*
+* Refrigerator
+* 냉장고 Entity
+*
+* @author hoony
+* @version 1.0.0
+* 생성일 2022-04-25
+* 마지막 수정일 2022-04-25
+**/
 public class Refrigerator {
 
     @Id
