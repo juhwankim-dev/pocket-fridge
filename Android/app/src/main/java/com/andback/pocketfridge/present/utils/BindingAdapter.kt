b874ee5122717d9a86a15fcd9bcdb1ef973ff9be
Juhwan Kim <juhwan.dev@gmail.com>
2022-04-25 15:58:49 +0900

    S06P31D206-31 [mo] feat: :sparkles: menu title에 따라 textChangedListener 설정

--- Android/app/src/main/java/com/andback/pocketfridge/present/utils/BindingAdapter.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/utils/BindingAdapter.kt
@@ -7,14 +7,39 @@ import androidx.core.widget.addTextChangedListener
 import androidx.databinding.BindingAdapter
 import androidx.databinding.InverseBindingAdapter
 import androidx.databinding.InverseBindingListener
+import com.andback.pocketfridge.R
 import com.andback.pocketfridge.present.views.user.UserViewModel
 
 object BindingAdapter {
     @JvmStatic
     @BindingAdapter("setTextWatcher")
     fun setTextWatcher(view: CustomForm, vm: UserViewModel) {
-        view.editText.addTextChangedListener {
-            vm.emailForSignUp.value = it.toString()
+        when(view.getMenuTitle()) {
+            "이메일" -> {
+                view.editText.addTextChangedListener {
+                    vm.emailForSignUp.value = it.toString()
+                }
+            }
+            "비밀번호" -> {
+                view.editText.addTextChangedListener {
+                    vm.pwForSignUp.value = it.toString()
+                }
+            }
+            "비밀번호 확인" -> {
+                view.editText.addTextChangedListener {
+                    vm.pwConfirmForSignUp.value = it.toString()
+                }
+            }
+            "이름" -> {
+                view.editText.addTextChangedListener {
+                    vm.nameForSignUp.value = it.toString()
+                }
+            }
+            "닉네임" -> {
+                view.editText.addTextChangedListener {
+                    vm.nicknameForSignUp.value = it.toString()
+                }
+            }
         }
     }
 }
