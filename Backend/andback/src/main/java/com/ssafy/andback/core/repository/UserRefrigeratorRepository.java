7c64b9b276e0f48afec229a177d99e0c686e7449
hoonyCode <rladudgns456@naver.com>
2022-04-25 10:44:08 +0900

    S06P31D206-164 [be] feat: :sparkles: 회원가입시 기본 냉장고 생성

---
+++ Backend/andback/src/main/java/com/ssafy/andback/core/repository/UserRefrigeratorRepository.java
@@ -0,0 +1,9 @@
+package com.ssafy.andback.core.repository;
+
+import com.ssafy.andback.core.domain.UserRefrigerator;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRefrigeratorRepository extends JpaRepository<UserRefrigerator, Long> {
+
+    
+}
