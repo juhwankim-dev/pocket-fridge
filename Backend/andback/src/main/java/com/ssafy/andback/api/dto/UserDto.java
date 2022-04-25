79293c6dbc25754d8afceadf49e0006647993d4f
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-04-25 11:56:42 +0900

    S06P31D206-33 [be] feat : :sparkles: 유효성 검사 기능 구현

--- Backend/andback/src/main/java/com/ssafy/andback/api/dto/UserDto.java
+++ Backend/andback/src/main/java/com/ssafy/andback/api/dto/UserDto.java
@@ -1,8 +1,12 @@
 package com.ssafy.andback.api.dto;
 
+import io.swagger.annotations.ApiParam;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 /**
  * UserDto
@@ -16,13 +20,27 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserDto {
-    @NonNull
+
+    @ApiParam(value = "유저 이메일", required = true)
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String userEmail;
-    @NonNull
+
+    @ApiParam(value = "유저 이름(2~20자)", required = true)
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣A-Za-z0-9-_]{2,20}$", message = "이름은 2~20자리여야 합니다.")
     private String userName;
-    @NonNull
+
+    @ApiParam(value = "유저 닉네임(특수문자 제외 2~10자)", required = true)
+    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣A-Za-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
     private String userNickname;
-    @NonNull
+
+    @ApiParam(value = "유저 비밀번호(영문자 포함한 5~15자)", required = true)
+    @NotBlank
+    @Pattern(regexp = "(?=.*[a-zA-Z]).{5,15}", message = "비밀번호는 영문자를 포함한 5~15자리여야 합니다.")
     private String userPassword;
+
+    @ApiParam(value = "유저 사진(null 허용")
     private String userPicture;
 }
