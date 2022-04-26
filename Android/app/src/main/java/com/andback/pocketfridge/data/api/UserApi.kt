d29a74a864d379a87f45cbf0fc760bc23815bb67
Juhwan Kim <juhwan.dev@gmail.com>
2022-04-26 15:59:40 +0900

    S06P31D206-31 [mo] fix: :bug: Flowable -> Observable로 변경

--- Android/app/src/main/java/com/andback/pocketfridge/data/api/UserApi.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/data/api/UserApi.kt
@@ -1,22 +1,21 @@
 package com.andback.pocketfridge.data.api
 
 import com.andback.pocketfridge.data.model.BaseResponse
-import io.reactivex.Flowable
+import io.reactivex.Observable
 import org.jetbrains.annotations.NotNull
-import retrofit2.Response
 import retrofit2.http.*
 
 interface UserApi {
     @FormUrlEncoded
-    @POST("user/signup")
-    fun signUp(@FieldMap req: MutableMap<String, String>): Flowable<BaseResponse<Any>>
+    @POST("user")
+    fun signUp(@FieldMap req: MutableMap<String, String>): Observable<BaseResponse<Any>>
 
     @GET("user/{userEmail}")
-    fun sendEmail(@Path("userEmail") @NotNull email: String): Flowable<BaseResponse<String>>
+    fun sendEmail(@Path("userEmail") @NotNull email: String): Observable<BaseResponse<String>>
 
     @GET("user/checkemail/{userEmail}")
-    fun checkEmail(@Path("userEmail") @NotNull email: String): Flowable<BaseResponse<Any>>
+    fun checkEmail(@Path("userEmail") @NotNull email: String): Observable<BaseResponse<Any>>
 
     @GET("user/checknickname/{userNickname}")
-    fun checkNickname(@Path("userNickname") @NotNull nickname: String): Flowable<BaseResponse<Any>>
+    fun checkNickname(@Path("userNickname") @NotNull nickname: String): Observable<BaseResponse<Any>>
 }
--- Android/app/src/main/java/com/andback/pocketfridge/data/repository/user/UserRemoteDataSourceImpl.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/data/repository/user/UserRemoteDataSourceImpl.kt
@@ -3,22 +3,23 @@ package com.andback.pocketfridge.data.repository.user
 import com.andback.pocketfridge.data.api.UserApi
 import com.andback.pocketfridge.data.model.BaseResponse
 import io.reactivex.Flowable
+import io.reactivex.Observable
 import javax.inject.Inject
 
 class UserRemoteDataSourceImpl @Inject constructor(private val userApi: UserApi) : UserRemoteDataSource {
-    override fun signUp(req: MutableMap<String, String>): Flowable<BaseResponse<Any>> {
+    override fun signUp(req: MutableMap<String, String>): Observable<BaseResponse<Any>> {
         return userApi.signUp(req)
     }
 
-    override fun sendEmail(email: String): Flowable<BaseResponse<String>> {
+    override fun sendEmail(email: String): Observable<BaseResponse<String>> {
         return userApi.sendEmail(email)
     }
 
-    override fun checkEmail(email: String): Flowable<BaseResponse<Any>> {
+    override fun checkEmail(email: String): Observable<BaseResponse<Any>> {
         return userApi.checkEmail(email)
     }
 
-    override fun checkNickname(nickname: String): Flowable<BaseResponse<Any>> {
+    override fun checkNickname(nickname: String): Observable<BaseResponse<Any>> {
         return userApi.checkNickname(nickname)
     }
 }
