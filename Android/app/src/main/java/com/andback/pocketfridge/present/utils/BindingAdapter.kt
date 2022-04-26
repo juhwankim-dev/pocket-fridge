1b74aa3fef46709e7d6386992a9b6cafd4e804e5
Juhwan Kim <juhwan.dev@gmail.com>
2022-04-26 17:20:58 +0900

    S06P31D206-31 [mo] style: :art: 사용하지 않는 import 삭제

--- Android/app/src/main/java/com/andback/pocketfridge/present/utils/BindingAdapter.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/utils/BindingAdapter.kt
@@ -1,15 +1,7 @@
 package com.andback.pocketfridge.present.utils
 
-import android.text.Editable
-import android.text.TextWatcher
-import android.widget.TextView
-import androidx.core.widget.addTextChangedListener
 import androidx.databinding.BindingAdapter
-import androidx.databinding.InverseBindingAdapter
-import androidx.databinding.InverseBindingListener
 import com.airbnb.lottie.LottieAnimationView
-import com.andback.pocketfridge.R
-import com.andback.pocketfridge.present.views.user.UserViewModel
 
 object BindingAdapter {
     @JvmStatic
