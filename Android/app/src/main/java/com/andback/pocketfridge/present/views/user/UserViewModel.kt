001011393242818cb4aff1354ba5e600f34b800b
Juhwan Kim <juhwan.dev@gmail.com>
2022-04-26 09:25:22 +0900

    S06P31D206-31 [mo] feat: :sparkles: viewModel 코드 정리, 중복 및 인증메일 기능 추가

--- Android/app/src/main/java/com/andback/pocketfridge/present/views/user/UserViewModel.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/user/UserViewModel.kt
@@ -4,8 +4,13 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.andback.pocketfridge.R
+import com.andback.pocketfridge.data.model.BaseResponse
 import com.andback.pocketfridge.domain.model.CheckResult
-import com.andback.pocketfridge.domain.usecase.GetLoginUseCase
+import com.andback.pocketfridge.domain.usecase.GetCheckEmailUseCase
+import com.andback.pocketfridge.domain.usecase.GetCheckNicknameUseCase
+import com.andback.pocketfridge.domain.usecase.GetSignUpUseCase
+import com.andback.pocketfridge.domain.usecase.GetSendEmailUseCase
 import com.andback.pocketfridge.present.config.SingleLiveEvent
 import com.andback.pocketfridge.present.utils.NetworkManager
 import com.andback.pocketfridge.present.utils.PageSet
@@ -21,27 +26,112 @@ import javax.inject.Inject
 
 @HiltViewModel
 class UserViewModel @Inject constructor (
-    private val getLoginUseCase: GetLoginUseCase,
-    private val networkManager: NetworkManager
+    private val getSignUpUseCase: GetSignUpUseCase,
+    private val getSendEmailUseCase: GetSendEmailUseCase,
+    private val getCheckEmailUseCase: GetCheckEmailUseCase,
+    private val getCheckNicknameUseCase: GetCheckNicknameUseCase
 ) : ViewModel() {
     private val compositeDisposable = CompositeDisposable()
 
     // viewmodel이 editText에 입력된 값을 observe 하기 위함
-    val emailForSignUp: MutableLiveData<String> = MutableLiveData("")
-    val pwForSignUp: MutableLiveData<String> = MutableLiveData("")
-    val pwConfirmForSignUp: MutableLiveData<String> = MutableLiveData("")
-    val nameForSignUp: MutableLiveData<String> = MutableLiveData("")
-    val nicknameForSignUp: MutableLiveData<String> = MutableLiveData("")
+    val email: MutableLiveData<String> = MutableLiveData("")
+    val name: MutableLiveData<String> = MutableLiveData("")
+    val nickname: MutableLiveData<String> = MutableLiveData("")
+    val pw: MutableLiveData<String> = MutableLiveData("")
+    val pwConfirm: MutableLiveData<String> = MutableLiveData("")
 
     // view가 다음으로 넘어갈 페이지를 observe 하기 위함
-//    private val _pageNumber = MutableLiveData<PageSet>()
-//    val pageNumber: LiveData<PageSet> get() = _pageNumber
-
     val pageNumber = SingleLiveEvent<PageSet>()
 
-    fun signUp(req: MutableMap<String, String>) {
+    // 이메일을 성공적으로 보냈는지 xml에서 확인하기 위함
+    val isSentEmail = SingleLiveEvent<Boolean>()
+
+    // 토스트 메시지 & 에러 메시지
+    private val _toastMsg = MutableLiveData<String>()
+    private val _toastMsgIntType = MutableLiveData<Int>()
+    private val _emailErrorMsg: MutableLiveData<CheckResult> = MutableLiveData()
+    private val _emailAuthNumberErrorMsg: MutableLiveData<Int> = MutableLiveData()
+    private val _nicknameErrorMsg: MutableLiveData<Int> = MutableLiveData()
+
+    val toastMsg: LiveData<String> get() = _toastMsg
+    val toastMsgIntType: LiveData<Int> get() = _toastMsgIntType
+    val emailErrorMsg: LiveData<CheckResult> get() = _emailErrorMsg
+    val emailAuthNumberErrorMsg: LiveData<Int> get() = _emailAuthNumberErrorMsg
+    val nicknameErrorMsg: LiveData<Int> get() = _nicknameErrorMsg
+
+    // 이메일 인증번호
+    val EmailAuthNumber: MutableLiveData<String> = MutableLiveData("")
+    var sentEmailAuthNumber = ""
+
+    private fun checkEmail(email: String) {
         compositeDisposable.add(
-            getLoginUseCase.execute(req)
+            getCheckEmailUseCase.execute(email)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    {
+                        when(it.status) {
+                            200 -> {
+                                _emailErrorMsg.value = CheckResult(R.string.no_error, true)
+                                sendEmail(email)
+                            }
+                            401 -> {
+                                _emailErrorMsg.value = CheckResult(R.string.email_overlap_error, false)
+                            }
+                        }
+                    }, {}, {}, {}
+                )
+        )
+    }
+
+    private fun sendEmail(email: String) {
+        compositeDisposable.add(
+            getSendEmailUseCase.execute(email)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    {
+                        when(it.status) {
+                            200 -> {
+                                isSentEmail.value = true
+                                sentEmailAuthNumber = it.data!!
+                                _toastMsg.value = it.message
+                            }
+                            401 -> {
+                                _toastMsg.value = it.message
+                            }
+                        }
+                    }, {}, {}, {}
+                )
+        )
+    }
+
+    private fun checkNickname(nickname: String) {
+        compositeDisposable.add(
+            getCheckNicknameUseCase.execute(nickname)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    {
+                        when(it.status) {
+                            200 -> {
+                                _nicknameErrorMsg.value = R.string.no_error
+                                signUp(getEnteredUserInfo())
+                                _toastMsg.value = it.message
+                            }
+                            401 -> {
+                                _nicknameErrorMsg.value = R.string.nickname_overlap_error
+                                _toastMsg.value = it.message
+                            }
+                        }
+                    }, {}, {}, {}
+                )
+        )
+    }
+
+    private fun signUp(req: MutableMap<String, String>) {
+        compositeDisposable.add(
+            getSignUpUseCase.execute(req)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
@@ -71,44 +161,31 @@ class UserViewModel @Inject constructor (
         pageNumber.value = PageSet.LOGIN
     }
 
+    fun onSendEmailClick() {
+        checkEmail(email.value!!)
+    }
+
     fun onNextClick() {
-        if (!checkNetworkState()) return
-
-        val id = emailForSignUp.value.toString()
-        val pw = pwForSignUp.value.toString()
-        val pwConfirm = pwConfirmForSignUp.value.toString()
-
-        if(SignUpChecker.validateEmail(id).isValid
-            && SignUpChecker.validatePw(pw).isValid
-            && SignUpChecker.validateConfirmPw(pw, pwConfirm).isValid) {
-            pageNumber.value = PageSet.STEP_TWO
+        if(sentEmailAuthNumber == EmailAuthNumber.value) {
+            _toastMsgIntType.value = R.string.email_auth_success
+            pageNumber.value = PageSet.SIGN_UP
+        } else {
+            _emailAuthNumberErrorMsg.value = R.string.email_auth_match_error
         }
     }
 
     fun onSignUpClick() {
-        if (!checkNetworkState()) return
-
-        val name = nameForSignUp.value.toString()
-        val nickname = nicknameForSignUp.value.toString()
-
-        if(SignUpChecker.validateName(name).isValid
-            && SignUpChecker.validateNickname(nickname).isValid) {
-            signUp(getEnteredUserInfo())
-        }
+        checkNickname(nickname.value!!)
     }
 
     private fun getEnteredUserInfo(): MutableMap<String, String>{
         val req = mutableMapOf<String, String>()
-        req["userEmail"] = emailForSignUp.value.toString()
-        req["userName"] = nameForSignUp.value.toString()
-        req["userNickname"] = nicknameForSignUp.value.toString()
-        req["userPassword"] = pwForSignUp.value.toString()
+        req["userEmail"] = email.value.toString()
+        req["userName"] = name.value.toString()
+        req["userNickname"] = nickname.value.toString()
+        req["userPassword"] = pw.value.toString()
 
         return req
-    }
-
-    private fun checkNetworkState(): Boolean {
-        return networkManager.checkNetworkState()
     }
 
     override fun onCleared() {
