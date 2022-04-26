8251fcefb519fd0532f2c0ced58d732a2922f601
김민수 <ms001118@naver.com>
2022-04-26 15:01:40 +0900

    S06P31D206-177 [mo] feat: :sparkles: FridgeApi 생성

---
+++ Android/app/src/main/java/com/andback/pocketfridge/data/api/FridgeApi.kt
@@ -0,0 +1,12 @@
+package com.andback.pocketfridge.data.api
+
+import com.andback.pocketfridge.data.model.BaseResponse
+import com.andback.pocketfridge.data.model.FridgeEntity
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface FridgeApi {
+    @GET("/refrigerator/{userEmail}")
+    fun getFridges(@Query("userEmail") email: String): Observable<BaseResponse<List<FridgeEntity>>>
+}
