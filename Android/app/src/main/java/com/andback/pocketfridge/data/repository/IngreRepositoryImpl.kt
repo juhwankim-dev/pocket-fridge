10f282e922e0274f82a90378863ede0b1a14cc6b
Juhwan Kim <juhwan.dev@gmail.com>
2022-04-26 09:14:13 +0900

    S06P31D206-31 [mo] fix : :bug: Ingre 관련 파일들에 있는 Single을 Observable로 변경

--- Android/app/src/main/java/com/andback/pocketfridge/data/repository/IngreRepositoryImpl.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/data/repository/IngreRepositoryImpl.kt
@@ -4,11 +4,12 @@ import com.andback.pocketfridge.data.model.BaseResponse
 import com.andback.pocketfridge.data.model.IngreEntityForUpload
 import com.andback.pocketfridge.data.repository.ingredient.IngreRemoteDataSource
 import com.andback.pocketfridge.domain.repository.IngreRepository
+import io.reactivex.Observable
 import io.reactivex.Single
 import javax.inject.Inject
 
 class IngreRepositoryImpl @Inject constructor(private val ingreRemoteDataSource: IngreRemoteDataSource): IngreRepository {
-    override fun uploadIngre(ingreEntityForUpload: IngreEntityForUpload): Single<BaseResponse<Any>> {
+    override fun uploadIngre(ingreEntityForUpload: IngreEntityForUpload): Observable<BaseResponse<Any>> {
         return ingreRemoteDataSource.uploadIngre(ingreEntityForUpload)
     }
 }
