c73c04c7c36b2ef6c8b73162896f3f2bb8d3599d
Juhwan Kim <juhwan.dev@gmail.com>
2022-04-26 09:13:11 +0900

    S06P31D206-31 [mo] fix : :bug: CustomView와 관련된 파일 삭제

--- Android/app/src/main/java/com/andback/pocketfridge/present/utils/BindingAdapter.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/utils/BindingAdapter.kt
@@ -11,35 +11,5 @@ import com.andback.pocketfridge.R
 import com.andback.pocketfridge.present.views.user.UserViewModel
 
 object BindingAdapter {
-    @JvmStatic
-    @BindingAdapter("setTextWatcher")
-    fun setTextWatcher(view: CustomForm, vm: UserViewModel) {
-        when(view.getMenuTitle()) {
-            "이메일" -> {
-                view.editText.addTextChangedListener {
-                    vm.emailForSignUp.value = it.toString()
-                }
-            }
-            "비밀번호" -> {
-                view.editText.addTextChangedListener {
-                    vm.pwForSignUp.value = it.toString()
-                }
-            }
-            "비밀번호 확인" -> {
-                view.editText.addTextChangedListener {
-                    vm.pwConfirmForSignUp.value = it.toString()
-                }
-            }
-            "이름" -> {
-                view.editText.addTextChangedListener {
-                    vm.nameForSignUp.value = it.toString()
-                }
-            }
-            "닉네임" -> {
-                view.editText.addTextChangedListener {
-                    vm.nicknameForSignUp.value = it.toString()
-                }
-            }
-        }
-    }
+
 }
