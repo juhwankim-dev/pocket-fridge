238b2692a299f549e3d1f55b2ff3dbe736a1e65d
Juhwan Kim <juhwan.dev@gmail.com>
2022-04-26 16:06:14 +0900

    S06P31D206-31 [mo] feat: :sparkles: Flowable -> Observable로 변경

--- Android/app/src/main/java/com/andback/pocketfridge/domain/usecase/GetCheckEmailUseCase.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/domain/usecase/GetCheckEmailUseCase.kt
@@ -1,10 +1,12 @@
 package com.andback.pocketfridge.domain.usecase
 
+import android.util.Log
 import com.andback.pocketfridge.data.model.BaseResponse
 import com.andback.pocketfridge.domain.repository.UserRepository
 import io.reactivex.Flowable
+import io.reactivex.Observable
 import javax.inject.Inject
 
 class GetCheckEmailUseCase @Inject constructor(val repository: UserRepository) {
-    fun execute(email: String): Flowable<BaseResponse<Any>> = repository.checkEmail(email)
+    fun execute(email: String): Observable<BaseResponse<Any>> = repository.checkEmail(email)
 }
--- Android/app/src/main/java/com/andback/pocketfridge/domain/usecase/GetCheckNicknameUseCase.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/domain/usecase/GetCheckNicknameUseCase.kt
@@ -2,9 +2,9 @@ package com.andback.pocketfridge.domain.usecase
 
 import com.andback.pocketfridge.data.model.BaseResponse
 import com.andback.pocketfridge.domain.repository.UserRepository
-import io.reactivex.Flowable
+import io.reactivex.Observable
 import javax.inject.Inject
 
 class GetCheckNicknameUseCase @Inject constructor(val repository: UserRepository) {
-    fun execute(nickname: String): Flowable<BaseResponse<Any>> = repository.checkNickname(nickname)
+    fun execute(nickname: String): Observable<BaseResponse<Any>> = repository.checkNickname(nickname)
 }
