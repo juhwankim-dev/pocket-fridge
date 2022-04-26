41351643845ec711aba116640a37e767b7f7e26c
Juhwan Kim <juhwan.dev@gmail.com>
2022-04-26 16:03:31 +0900

    S06P31D206-31 [mo] feat: :sparkles: Lottie 플레이 속성 추가

--- Android/app/src/main/java/com/andback/pocketfridge/present/utils/BindingAdapter.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/utils/BindingAdapter.kt
@@ -7,9 +7,17 @@ import androidx.core.widget.addTextChangedListener
 import androidx.databinding.BindingAdapter
 import androidx.databinding.InverseBindingAdapter
 import androidx.databinding.InverseBindingListener
+import com.airbnb.lottie.LottieAnimationView
 import com.andback.pocketfridge.R
 import com.andback.pocketfridge.present.views.user.UserViewModel
 
 object BindingAdapter {
-
+    @JvmStatic
+    @BindingAdapter("setLottiePlay")
+    fun setLottiePlay(view: LottieAnimationView, isShowLoading: Boolean){
+        when(isShowLoading) {
+            true -> view.playAnimation()
+            false -> view.pauseAnimation()
+        }
+    }
 }
