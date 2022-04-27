2e3cd63af8c662a70ac18198b31d5abc22cf43cd
Juhwan Kim <juhwan.dev@gmail.com>
2022-04-27 17:11:58 +0900

    S06P31D206-35 [mo] fix: :but: 오타 수정

--- Android/app/src/main/java/com/andback/pocketfridge/present/views/user/UserActivity.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/user/UserActivity.kt
@@ -1,19 +1,22 @@
 package com.andback.pocketfridge.present.views.user
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.andback.pocketfridge.R
 import com.andback.pocketfridge.databinding.ActivityUserBinding
 import com.andback.pocketfridge.present.config.BaseActivity
 import com.andback.pocketfridge.present.utils.PageSet
+import com.andback.pocketfridge.present.views.main.MainActivity
+import com.andback.pocketfridge.present.views.user.login.LoginFragment
 import com.andback.pocketfridge.present.views.user.signup.EmailAuthFragment
 import com.andback.pocketfridge.present.views.user.signup.SignUpFragment
-import com.andback.pocketfridge.present.views.user.signup.SignViewModel
+import com.andback.pocketfridge.present.views.user.signup.SignUpViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class UserActivity : BaseActivity<ActivityUserBinding>(R.layout.activity_user) {
-    private val viewModel: SignViewModel by viewModels()
+    private val upViewModel: SignUpViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +25,7 @@ class UserActivity : BaseActivity<ActivityUserBinding>(R.layout.activity_user) {
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fl_main)
 
         if(currentFragment == null){
-            val fragment = EmailAuthFragment()
+            val fragment = LoginFragment()
             supportFragmentManager
                 .beginTransaction()
                 .add(R.id.fl_main, fragment)
@@ -42,9 +45,12 @@ class UserActivity : BaseActivity<ActivityUserBinding>(R.layout.activity_user) {
                 .replace(R.id.fl_main, SignUpFragment())
                 .addToBackStack(null)
                 .commit()
-            PageSet.LOGIN -> {
-                // Login Activity로 이동
-            }
+            PageSet.LOGIN -> supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fl_main, LoginFragment())
+                .addToBackStack(null)
+                .commit()
+            PageSet.MAIN -> startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
