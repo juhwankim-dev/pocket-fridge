11bfa9376a96983a2689368253a94637dbfc7919
김민수 <ms001118@naver.com>
2022-04-28 15:33:54 +0900

    S06P31D206-238 [mo] fix: viewmodel 필드 수정에 따른 리팩터링

--- Android/app/src/main/java/com/andback/pocketfridge/present/views/main/IngreUploadFragment.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/IngreUploadFragment.kt
@@ -3,16 +3,11 @@ package com.andback.pocketfridge.present.views.main
 import android.app.DatePickerDialog
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
-import android.widget.DatePicker
 import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import com.andback.pocketfridge.R
 import com.andback.pocketfridge.data.model.FridgeEntity
@@ -28,10 +23,6 @@ class IngreUploadFragment : BaseFragment<FragmentIngreUploadBinding>(R.layout.fr
         private const val TAG = "IngreUploadFragment_debuk"
     }
     private val viewModel: IngreUploadViewModel by activityViewModels()
-    private val fridgeViewModel: FridgeViewModel by activityViewModels()
-    // TODO: 냉장고 목록 요청
-    // TODO: 냉장고 드롭박스 어댑터
-    // TODO: app bar navbtn, action 세팅
     // TODO: upload
     // TODO: 입력값 에러처리
 
@@ -80,24 +71,20 @@ class IngreUploadFragment : BaseFragment<FragmentIngreUploadBinding>(R.layout.fr
                 }
 
             // 식재료 정보관련 live data
-                ingreStorage.observe(owner) {
+                selectedStorage.observe(owner) {
                     clearStorageTextView()
                     setBrandColorOnText(it)
                 }
 
-                ingreCategory.observe(owner) {
-                    binding.tvIngreUploadFCategory.text = it
+                selectedSubCategory.observe(owner) {
+                    binding.tvIngreUploadFCategory.text = it.subCategoryName
                 }
 
-                // 서브카테고리 리스트가 있으면 첫 값을 기본으로 세팅
-                subCategory.observe(owner) {
-                    ingreCategory.value = it[0].subCategoryName
-                }
-            }
-        }
-
-        with(fridgeViewModel) {
-            binding.lifecycleOwner?.let { owner ->
+//                // 서브카테고리 리스트가 있으면 첫 값을 기본으로 세팅
+//                subCategories.observe(owner) {
+//                    ingreCategory.value = it[0].subCategoryName
+//                }
+            // 냉장고 리스트 세팅
                 fridges.observe(owner) {
                     setDropDownAdapter(it)
                 }
@@ -125,7 +112,7 @@ class IngreUploadFragment : BaseFragment<FragmentIngreUploadBinding>(R.layout.fr
         binding.tilIngreUploadFExpiryDate.setStartIconOnClickListener {
             showDatePickerWith { _, year, month, day ->
                 val result = DateConverter.toStringDate(year, month, day)
-                viewModel.ingreDateExpiry.value = result
+                viewModel.dateExpiry.value = result
                 Log.d(TAG, "setExpiryDateIcon: $result")
             }
         }
@@ -135,7 +122,7 @@ class IngreUploadFragment : BaseFragment<FragmentIngreUploadBinding>(R.layout.fr
         binding.tilIngreUploadFPurchasedDate.setStartIconOnClickListener {
             showDatePickerWith { _, year, month, day ->
                 val result = DateConverter.toStringDate(year, month, day)
-                viewModel.ingreDatePurchased.value = result
+                viewModel.datePurchased.value = result
                 Log.d(TAG, "setPurchasedDateIcon: $result")
             }
         }
@@ -159,7 +146,7 @@ class IngreUploadFragment : BaseFragment<FragmentIngreUploadBinding>(R.layout.fr
                 val fridge = list.find { it.refrigeratorName == text.toString() }
                 Log.d(TAG, "setDropDownAdapter: $fridge")
                 // 냉장고 이름으로 FridgeEntity 찾아서 viewmodel에 update
-                viewModel.ingreFridgeId.value = fridge?.refrigeratorId
+                fridge?.let { viewModel.setFridge(it) }
             }
             tv.setAdapter(adapter)
             // 기본값으로 첫번째 FridgeEntity 세팅
