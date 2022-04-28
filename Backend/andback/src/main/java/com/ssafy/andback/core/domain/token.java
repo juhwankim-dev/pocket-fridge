80f6835f42a97b8abe28f464c219b702239b5506
hoonyCode <rladudgns456@naver.com>
2022-04-28 15:07:43 +0900

    S06P31D206-200 [be] feat: :sparkles:  token Entity 생성 - token.java 생성

--- Backend/andback/src/main/java/com/ssafy/andback/core/domain/token.java
+++ Backend/andback/src/main/java/com/ssafy/andback/core/domain/token.java
@@ -1,2 +1,35 @@
-package com.ssafy.andback.core.domain;public class token {
+package com.ssafy.andback.core.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+/**
+*
+* token
+* Token Entity 생성!
+*
+* @author hoony
+* @version 1.0.0
+* 생성일 2022-04-28
+* 마지막 수정일 2022-04-28
+**/
+
+@Entity
+@Getter
+@Setter
+public class token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
+    @Column(name = "token_id")
+    private Long tokenId;
+
+    @Column(name = "token_device")
+    private String tokenDevice;
+
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User userId;
 }
