8a747f7d81ad75c733609646a50e768ff664d199
김민수 <ms001118@naver.com>
2022-04-29 15:53:56 +0900

    S06P31D206-242 [mo] feat: :sparkles: 냉장고 아이디로 식재료 요청 함수 생성

--- Android/app/src/main/java/com/andback/pocketfridge/data/api/IngreApi.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/data/api/IngreApi.kt
@@ -1,13 +1,19 @@
 package com.andback.pocketfridge.data.api
 
 import com.andback.pocketfridge.data.model.BaseResponse
+import com.andback.pocketfridge.data.model.IngreEntity
 import com.andback.pocketfridge.data.model.IngreEntityForUpload
 import io.reactivex.Observable
 import io.reactivex.Single
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface IngreApi {
     @POST("foodingredient")
     fun uploadIngre(@Body ingredient: IngreEntityForUpload): Observable<BaseResponse<Any>>
+
+    @GET("foodingredient/{refrigeratorId}")
+    fun getIngreListByFridgeId(@Path("refrigeratorId") fridgeId: Int): Single<BaseResponse<List<IngreEntity>>>
 }
