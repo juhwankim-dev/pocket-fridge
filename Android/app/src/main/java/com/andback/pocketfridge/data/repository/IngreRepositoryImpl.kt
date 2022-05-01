92564b9aa1cad837943e22dd1d533530e0860daa
김민수 <ms001118@naver.com>
2022-05-01 14:19:19 +0900

    S06P31D206-242 [mo] fix: :bug: IngreRepository에서 매퍼 사용

--- Android/app/src/main/java/com/andback/pocketfridge/data/repository/IngreRepositoryImpl.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/data/repository/IngreRepositoryImpl.kt
@@ -1,20 +1,29 @@
 package com.andback.pocketfridge.data.repository
 
+import com.andback.pocketfridge.data.mapper.IngreMapper
 import com.andback.pocketfridge.data.model.BaseResponse
 import com.andback.pocketfridge.data.model.IngreEntity
 import com.andback.pocketfridge.data.model.IngreEntityForUpload
 import com.andback.pocketfridge.data.repository.ingredient.IngreRemoteDataSource
+import com.andback.pocketfridge.domain.model.Ingredient
 import com.andback.pocketfridge.domain.repository.IngreRepository
 import io.reactivex.Observable
 import io.reactivex.Single
 import javax.inject.Inject
 
 class IngreRepositoryImpl @Inject constructor(private val ingreRemoteDataSource: IngreRemoteDataSource): IngreRepository {
+    // TODO: 매퍼 적용 필요 
     override fun uploadIngre(ingreEntityForUpload: IngreEntityForUpload): Observable<BaseResponse<Any>> {
         return ingreRemoteDataSource.uploadIngre(ingreEntityForUpload)
     }
 
-    override fun getIngreListByFridgeId(fridgeId: Int): Single<BaseResponse<List<IngreEntity>>> {
-        return ingreRemoteDataSource.getIngreListByFridgeId(fridgeId)
+    override fun getIngreListByFridgeId(fridgeId: Int): Single<BaseResponse<List<Ingredient>>> {
+        val result = ingreRemoteDataSource.getIngreListByFridgeId(fridgeId)
+        return result.map {
+            val list = it.data?.map { entity ->
+                IngreMapper(entity)
+            }
+            BaseResponse(message = it.message, status = it.status, data = list)
+        }
     }
 }
--- Android/app/src/main/java/com/andback/pocketfridge/domain/repository/IngreRepository.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/domain/repository/IngreRepository.kt
@@ -3,10 +3,11 @@ package com.andback.pocketfridge.domain.repository
 import com.andback.pocketfridge.data.model.BaseResponse
 import com.andback.pocketfridge.data.model.IngreEntity
 import com.andback.pocketfridge.data.model.IngreEntityForUpload
+import com.andback.pocketfridge.domain.model.Ingredient
 import io.reactivex.Observable
 import io.reactivex.Single
 
 interface IngreRepository {
     fun uploadIngre(ingreEntityForUpload: IngreEntityForUpload): Observable<BaseResponse<Any>>
-    fun getIngreListByFridgeId(fridgeId: Int): Single<BaseResponse<List<IngreEntity>>>
+    fun getIngreListByFridgeId(fridgeId: Int): Single<BaseResponse<List<Ingredient>>>
 }
