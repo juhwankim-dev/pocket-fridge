d4bca9b5fb30bfe10f2ff45a322b5c2e4823ed31
김민수 <ms001118@naver.com>
2022-05-02 17:59:36 +0900

    S06P31D206-246 [mo] feat: :sparkles: String으로 Enum 탐색 함수 추가

--- Android/app/src/main/java/com/andback/pocketfridge/present/utils/Storage.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/utils/Storage.kt
@@ -1,5 +1,13 @@
 package com.andback.pocketfridge.present.utils
 
+import android.util.Log
+
 enum class Storage(val value: String) {
-    Fridge("냉장"), Freeze("냉동"), Room("실온")
+    Fridge("냉장"), Freeze("냉동"), Room("실온");
+
+    companion object {
+        private val map = values().associateBy(Storage::value)
+        fun getWithString(string: String) = map[string]?: throw IllegalArgumentException()
+    }
+
 }
