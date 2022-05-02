2b8baa20170911ef465b279d47007d87d9ef276e
Juhwan Kim <juhwan.dev@gmail.com>
2022-05-02 10:20:23 +0900

    S06P31D206-205 [mo] feat: :sparkles: 단계별 레시피 프래그먼트 추가

---
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/recipe/detail/RecipeStepsFragment.kt
@@ -0,0 +1,9 @@
+package com.andback.pocketfridge.present.views.main.recipe.detail
+
+import com.andback.pocketfridge.R
+import com.andback.pocketfridge.databinding.FragmentRecipeStepsBinding
+import com.andback.pocketfridge.present.config.BaseFragment
+
+class RecipeStepsFragment : BaseFragment<FragmentRecipeStepsBinding>(R.layout.fragment_recipe_steps) {
+
+}
