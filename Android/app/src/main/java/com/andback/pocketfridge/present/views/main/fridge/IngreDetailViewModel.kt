f0729a66376be860891bfbf94e3ebc33516d2ab4
김민수 <ms001118@naver.com>
2022-05-03 22:31:42 +0900

    S06P31D206-252 [mo] fix: :bug: IngreDetailViewModel 뷰모델 상속 추가

--- Android/app/src/main/java/com/andback/pocketfridge/present/views/main/fridge/IngreDetailViewModel.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/fridge/IngreDetailViewModel.kt
@@ -2,6 +2,7 @@ package com.andback.pocketfridge.present.views.main.fridge
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.andback.pocketfridge.domain.model.Ingredient
 import com.andback.pocketfridge.domain.usecase.ingredient.DeleteIngreUseCase
 import com.andback.pocketfridge.domain.usecase.ingredient.UpdateIngreUseCase
@@ -13,7 +14,7 @@ import javax.inject.Inject
 @HiltViewModel
 class IngreDetailViewModel @Inject constructor(
     private val deleteIngreUseCase: DeleteIngreUseCase,
-) {
+): ViewModel() {
     private val _selectedIngre = MutableLiveData<Ingredient>()
     val selectedIngre: LiveData<Ingredient> = _selectedIngre
     
