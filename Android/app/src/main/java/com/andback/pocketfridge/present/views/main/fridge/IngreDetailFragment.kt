1630da23e2a7843bf55ec0fc4e5f6020a5c1eb4c
김민수 <ms001118@naver.com>
2022-05-03 14:15:37 +0900

    S06P31D206-250 [mo] feat: :sparkles: IngreDetailFragment 생성

---
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/fridge/IngreDetailFragment.kt
@@ -0,0 +1,13 @@
+package com.andback.pocketfridge.present.views.main.fridge
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.andback.pocketfridge.R
+import com.andback.pocketfridge.databinding.FragmentIngreDetailBinding
+import com.andback.pocketfridge.present.config.BaseFragment
+
+class IngreDetailFragment : BaseFragment<FragmentIngreDetailBinding>(R.layout.fragment_ingre_detail) {
+}
