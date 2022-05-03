5a2296efd8d19252f4c505479c824bf5be8d37dd
김민수 <ms001118@naver.com>
2022-05-04 08:58:26 +0900

    S06P31D206-252 [mo] feat: :sparkles: IngreDetailFragment 생성

--- Android/app/src/main/java/com/andback/pocketfridge/present/views/main/fridge/IngreDetailFragment.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/fridge/IngreDetailFragment.kt
@@ -1,13 +1,78 @@
 package com.andback.pocketfridge.present.views.main.fridge
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.andback.pocketfridge.R
 import com.andback.pocketfridge.databinding.FragmentIngreDetailBinding
+import com.andback.pocketfridge.domain.model.Ingredient
 import com.andback.pocketfridge.present.config.BaseFragment
 
 class IngreDetailFragment : BaseFragment<FragmentIngreDetailBinding>(R.layout.fragment_ingre_detail) {
+    private val viewModel: IngreDetailViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        arguments?.let {
+            viewModel.selectIngre(it["data"] as Ingredient)
+        }?: return
+        initView()
+        setObserver()
+        setToolbar()
+    }
+
+    private fun setObserver() {
+        with(viewModel) {
+            binding.lifecycleOwner?.let { owner ->
+                selectedIngre.observe(owner) {
+                    setView(it)
+                }
+                subCategory.observe(owner) {
+                    binding.tvIngreDetailFCategory.text = it.subCategoryName
+                }
+            }
+        }
+    }
+    
+    private fun initView() {
+        binding.btnIngreDetailFDelete.setOnClickListener { 
+            viewModel.deleteIngre()
+        }
+        binding.btnIngreDetailFEdit.setOnClickListener {
+            // TODO: 수정 fragment로 이동 
+        }
+    }
+
+    private fun setView(ingredient: Ingredient) {
+        binding.ingredient = ingredient
+        binding.leftDay = when {
+            ingredient.leftDay == 0 -> {
+                "D-Day"
+            }
+            ingredient.leftDay < 0 -> {
+                "D${ingredient.leftDay}"
+            }
+            else -> {
+                "D+${ingredient.leftDay}"
+            }
+        }
+    }
+
+    private fun setToolbar() {
+        binding.tbIngreDetailF.setNavigationOnClickListener {
+            requireActivity().onBackPressed()
+        }
+    }
+    
+    companion object {
+        private const val TAG = "IngreDetailFragment_debuk"
+    }
 }
