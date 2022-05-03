d020979aaf1db8e2b14fde957d77f9fbbb32fa9a
mdown <mdown@naver.com>
2022-05-04 00:45:34 +0900

    S06P31D206-206 [mo] style: :art: STT, TTS 부분 주석 추가

--- Android/app/src/main/java/com/andback/pocketfridge/present/utils/TTSUtil.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/utils/TTSUtil.kt
@@ -10,6 +10,7 @@ import java.util.*
 
 class TTSUtil(val context: Context, val stt: STTUtil?, val isListening: MutableLiveData<Boolean>) {
     lateinit var textToSpeech: TextToSpeech
+    // "시리야"처럼 음성비서를 호출하면 true, 그 외 음성이 들어오면 false
     private var isReady = false
 
     fun initTTS() {
@@ -22,13 +23,23 @@ class TTSUtil(val context: Context, val stt: STTUtil?, val isListening: MutableL
             }
         }
 
+        // Utterance = 발언
+        // 발언의 진행 단계를 알려주는 리스너
         textToSpeech.setOnUtteranceProgressListener(object: UtteranceProgressListener() {
+            // 발언이 시작되기 전 호출
             override fun onStart(p0: String?) { }
 
+            // 발언을 마친 후 호출
             override fun onDone(p0: String?) {
                 val mainHandler = Handler(context.mainLooper)
                 val myRunnable = Runnable {
+                    // "네 말씀하세요" 발언이 끝난 후 사용자로부터 음성 입력을 받기 위해 stt를 시작
                     stt!!.startListening()
+
+                    // 음성 비서를 호출했다면(= isReady가 true면) 듣는 중으로 상태 변경(= isListening을 true로)
+                    // 호출하지 않았다면 false가 들어감
+                    // 여기서 `듣는 중`이란 화면에 파형이 나오며 사용자에게 듣는 중임을 알리기 위한 변수임
+                    // 음성비서는 항상 듣고 있으니 의미를 혼동하지 말것
                     isListening.postValue(isReady)
                 }
                 mainHandler.post(myRunnable)
@@ -39,8 +50,14 @@ class TTSUtil(val context: Context, val stt: STTUtil?, val isListening: MutableL
     }
 
     fun speak(text: String) {
+        // "시리야"처럼 음성 비서를 호출하면 isReady를 true로 하는게 맞으나
+        // stt의 값을 가져오기에는 구조가 복잡해지기 때문에
+        // "시리야"를 부르면 응답하는 멘트인 "네 말씀해주세요"로 isReady를 설정
         isReady = (text == context.resources.getString(R.string.ipa_ready))
+
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            // 4번째 값에 null을 넣으면 setOnUtteranceProgressListener가 호출되지 않기 때문에
+            // 임의의 값을 설정함
             textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, "temp")
         } else {
             textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null)
--- Android/app/src/main/java/com/andback/pocketfridge/present/views/main/recipe/detail/RecipeStepsFragment.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/recipe/detail/RecipeStepsFragment.kt
@@ -48,6 +48,9 @@ class RecipeStepsFragment : BaseFragment<FragmentRecipeStepsBinding>(R.layout.fr
     }
 
     private fun initEvent() {
+        // 마이크 버튼을 클릭하면 음성 비서가 응답
+        // 원래는 음성비서를 부르면 -> command에 "xx아"가 들어가고 -> 응답을 함
+        // 버튼을 누르면 -> command에 "xx아"를 직접 넣어주어서 -> 응답을 하게 만듦
         binding.ivRecipeStepsFCircle.setOnClickListener {
             assistant.command = resources.getStringArray(R.array.ipa_name_list)[0]
         }
@@ -63,17 +66,30 @@ class RecipeStepsFragment : BaseFragment<FragmentRecipeStepsBinding>(R.layout.fr
         tts.initTTS()
     }
 
+    // command 만으로는 Util에 값을 넘길 수 없기 때문에 inner class로 감쌌음
     inner class Assistant {
         var command: String by Delegates.observable("") {
                 _, oldValue, newValue ->
+
+            // if(비서를 부르는 메시지라면) speak(대답)
             if (isCallMsg(newValue)) {
                 tts.speak(resources.getString(R.string.ipa_ready))
-            } else if (newValue == "다음" && isCallMsg(oldValue)) {
+            }
+
+            // if(현재 "다음"을 말했고 && 이전에 비서를 불렀었다면) speak(대답)
+            else if (newValue == "다음" && isCallMsg(oldValue)) {
                 tts.speak(resources.getString(R.string.ipa_next))
-            } else {
+            }
+
+            // 그 외
+            // ex) 비서를 부르거나 다음을 말하지 않은 경우
+            // ex) 다음을 말했으나 이전에 비서를 부르지 않은 경우
+            else {
                 val mainHandler = Handler(requireContext().mainLooper)
                 val myRunnable = Runnable {
+                    // 다음 음성 호출을 기다림
                     stt.startListening()
+                    // 듣고 있지 않음으로 설정(= 음성 파형 보여주지 않음, xml에 설정되어 있음)
                     isListening.postValue(false)
                 }
                 mainHandler.post(myRunnable)
@@ -81,6 +97,9 @@ class RecipeStepsFragment : BaseFragment<FragmentRecipeStepsBinding>(R.layout.fr
         }
     }
 
+    // 영훈아, 영후나, 영운아 등
+    // "xx아"와 비슷한 말들을 string에 저장해두었고
+    // 이와 일치하는 음성이 들어오면 음성 비서를 호출한 것으로 판단 (return true)
     private fun isCallMsg(s: String): Boolean {
         return resources.getStringArray(R.array.ipa_name_list).any { s.indexOf(it) > -1 }
     }
