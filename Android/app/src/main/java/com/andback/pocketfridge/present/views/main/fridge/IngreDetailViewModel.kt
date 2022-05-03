f65b341bb1d9b16ec096188ecddf37331f5b703a
김민수 <ms001118@naver.com>
2022-05-03 17:35:42 +0900

    S06P31D206-252 [mo] feat: :sparkles: IngreDetailViewModel 생성

---
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/fridge/IngreDetailViewModel.kt
@@ -0,0 +1,38 @@
+package com.andback.pocketfridge.present.views.main.fridge
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.andback.pocketfridge.domain.model.Ingredient
+import com.andback.pocketfridge.domain.usecase.ingredient.DeleteIngreUseCase
+import com.andback.pocketfridge.domain.usecase.ingredient.UpdateIngreUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
+
+@HiltViewModel
+class IngreDetailViewModel @Inject constructor(
+    private val deleteIngreUseCase: DeleteIngreUseCase,
+) {
+    private val _selectedIngre = MutableLiveData<Ingredient>()
+    val selectedIngre: LiveData<Ingredient> = _selectedIngre
+    
+    fun selectIngre(ingre: Ingredient) {
+        _selectedIngre.value = ingre
+    }
+    
+    fun deleteIngre() {
+        selectedIngre.value?.let { 
+            deleteIngreUseCase(it.id).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    {
+                        // TODO: 삭제 성공
+                    },
+                    {
+                        // TODO: 예외 처리 
+                    }
+                )
+        }
+    }
+}
