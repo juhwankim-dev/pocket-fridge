a74bebf28f09ad8257e5f02fa7ac89307a02569d
김민수 <ms001118@naver.com>
2022-05-03 15:41:07 +0900

    S06P31D206-251 [mo] fix: :bug: UpdateIngreUseCase 수정

--- Android/app/src/main/java/com/andback/pocketfridge/domain/usecase/ingredient/UpdateIngreUseCase.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/domain/usecase/ingredient/UpdateIngreUseCase.kt
@@ -1,13 +1,15 @@
 package com.andback.pocketfridge.domain.usecase.ingredient
 
+import com.andback.pocketfridge.data.model.BaseResponse
 import com.andback.pocketfridge.domain.model.Ingredient
 import com.andback.pocketfridge.domain.repository.IngreRepository
+import io.reactivex.Single
 import javax.inject.Inject
 
 class UpdateIngreUseCase @Inject constructor(
     private val ingreRepository: IngreRepository
 ) {
-    operator fun invoke(ingredient: Ingredient) {
-        ingreRepository.updateIngre(ingredient.id, ingredient)
+    operator fun invoke(ingredient: Ingredient): Single<BaseResponse<Any>> {
+        return ingreRepository.updateIngre(ingredient.id, ingredient)
     }
 }
