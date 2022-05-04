84bb119c529abb68556bf8c1c6b00520dd0b469a
mdown <mdown@naver.com>
2022-05-04 13:17:21 +0900

    S06P31D206-205 [mo] feat: :sparkles: 레시피 관련 useCase 작성 및 수정

--- Android/app/src/main/java/com/andback/pocketfridge/domain/usecase/recipe/GetCookingIngresUseCase.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/domain/usecase/recipe/GetCookingIngresUseCase.kt
@@ -1,13 +1,14 @@
 package com.andback.pocketfridge.domain.usecase.recipe
 
 import com.andback.pocketfridge.data.model.BaseResponse
-import com.andback.pocketfridge.data.model.RecipeStepEntity
+import com.andback.pocketfridge.data.model.CookingIngreEntity
 import com.andback.pocketfridge.domain.repository.RecipeRepository
 import io.reactivex.Single
 import javax.inject.Inject
 
-class GetRecipeStepsUseCase @Inject constructor(
+class GetCookingIngresUseCase @Inject constructor(
     private val recipeRepository: RecipeRepository
 ) {
-    operator fun invoke(recipeId: Int): Single<BaseResponse<List<RecipeStepEntity>>> = recipeRepository.getRecipeSteps(recipeId)
+    operator fun invoke(recipeId: Int): Single<BaseResponse<List<CookingIngreEntity>>> =
+        recipeRepository.getCookingIngres(recipeId)
 }
--- Android/app/src/main/java/com/andback/pocketfridge/domain/usecase/recipe/GetRecipesUseCase.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/domain/usecase/recipe/GetRecipesUseCase.kt
@@ -2,14 +2,12 @@ package com.andback.pocketfridge.domain.usecase.recipe
 
 import com.andback.pocketfridge.data.model.BaseResponse
 import com.andback.pocketfridge.data.model.RecipeEntity
-import com.andback.pocketfridge.data.model.RecipeStepEntity
 import com.andback.pocketfridge.domain.repository.RecipeRepository
-import io.reactivex.Observable
 import io.reactivex.Single
 import javax.inject.Inject
 
 class GetRecipesUseCase @Inject constructor(
     private val recipeRepository: RecipeRepository
 ) {
-    fun getRecipes(): Single<BaseResponse<List<RecipeEntity>>> = recipeRepository.getRecipes()
+    operator fun invoke(): Single<BaseResponse<List<RecipeEntity>>> = recipeRepository.getRecipes()
 }
