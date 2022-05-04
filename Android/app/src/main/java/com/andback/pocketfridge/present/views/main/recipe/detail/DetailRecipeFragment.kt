0d5e8f0ae94f39db93ef84262e871a02f83a000c
mdown <mdown@naver.com>
2022-05-05 02:16:04 +0900

    S06P31D206-205 [mo] feat: :sparkles: 임시 데이터 삭제 및 실제 데이터 반영 로직 작성 (UI Controller)

--- Android/app/src/main/java/com/andback/pocketfridge/present/views/main/recipe/detail/DetailRecipeFragment.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/recipe/detail/DetailRecipeFragment.kt
@@ -3,31 +3,44 @@ package com.andback.pocketfridge.present.views.main.recipe.detail
 import android.os.Bundle
 import android.view.View
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.andback.pocketfridge.R
 import com.andback.pocketfridge.databinding.FragmentDetailRecipeBinding
-import com.andback.pocketfridge.domain.model.Recipe
 import com.andback.pocketfridge.present.config.BaseFragment
 import com.andback.pocketfridge.present.views.main.recipe.RecipeItemDecoration
 
 class DetailRecipeFragment : BaseFragment<FragmentDetailRecipeBinding>(R.layout.fragment_detail_recipe) {
     lateinit var detailRecipeAdapter: DetailRecipeAdapter
+    private val viewModel: CookSharedViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initView()
+        initViewModel()
     }
 
     private fun initView() {
-        // API 만들어지기 전까지 임시
         detailRecipeAdapter = DetailRecipeAdapter()
-        detailRecipeAdapter.setList(listOf(Recipe(""), Recipe("")))
+        detailRecipeAdapter.setHeaderContent(viewModel.selectedRecipe)
 
         binding.rvDetailRecipe.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = detailRecipeAdapter
             addItemDecoration(RecipeItemDecoration(20F, ContextCompat.getColor(context, R.color.gray_divider)))
+        }
+    }
+
+    private fun initViewModel() {
+        with(viewModel) {
+            toastMsg.observe(viewLifecycleOwner) {
+                showToastMessage(it)
+            }
+
+            cookingIngres.observe(viewLifecycleOwner) {
+                detailRecipeAdapter.setBodyContent(it)
+            }
         }
     }
 }
