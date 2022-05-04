9a259da5c9a5c196baa017dd83887076bf4462dc
김민수 <ms001118@naver.com>
2022-05-04 15:58:46 +0900

    S06P31D206-253 [mo] fix: :bug: 식재료 업데이트 유효성 검사 추가

--- Android/app/src/main/java/com/andback/pocketfridge/domain/usecase/ingredient/UpdateIngreUseCase.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/domain/usecase/ingredient/UpdateIngreUseCase.kt
@@ -1,8 +1,10 @@
 package com.andback.pocketfridge.domain.usecase.ingredient
 
 import com.andback.pocketfridge.data.model.BaseResponse
+import com.andback.pocketfridge.data.model.IngreEntityForUpload
 import com.andback.pocketfridge.domain.model.Ingredient
 import com.andback.pocketfridge.domain.repository.IngreRepository
+import com.andback.pocketfridge.present.utils.IngreChecker
 import io.reactivex.Single
 import javax.inject.Inject
 
@@ -10,6 +12,21 @@ class UpdateIngreUseCase @Inject constructor(
     private val ingreRepository: IngreRepository
 ) {
     operator fun invoke(ingredient: Ingredient): Single<BaseResponse<Any>> {
-        return ingreRepository.updateIngre(ingredient)
+        return checkValidationThenUpload(ingredient)
+    }
+
+    private fun checkValidationThenUpload(ingredient: Ingredient): Single<BaseResponse<Any>> {
+        return try {
+            IngreChecker.check(ingredient)
+            ingreRepository.updateIngre(ingredient)
+        } catch (e: Throwable) {
+            makeValidationErrorObservable(e)
+        }
+    }
+
+    private fun makeValidationErrorObservable(e: Throwable): Single<BaseResponse<Any>> {
+        return Single.create { emitter ->
+            emitter.onError(e)
+        }
     }
 }
