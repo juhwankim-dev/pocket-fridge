e224057eb3072e27c2e79c57ff999b66281779c7
김민수 <ms001118@naver.com>
2022-05-04 11:46:47 +0900

    S06P31D206-253 [mo] feat: :sparkles: IngreEditFragment 생성

---
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/fridge/IngreEditFragment.kt
@@ -0,0 +1,15 @@
+package com.andback.pocketfridge.present.views.main.fridge
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.andback.pocketfridge.R
+import com.andback.pocketfridge.databinding.FragmentIngreEditBinding
+import com.andback.pocketfridge.present.config.BaseFragment
+
+class IngreEditFragment: BaseFragment<FragmentIngreEditBinding>(R.layout.fragment_ingre_edit) {
+
+}
