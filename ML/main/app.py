16fb29067f1903da9df47a7da797ddc16b0f65ac
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-05-06 10:51:20 +0900

    S06P31D206-219 [be] chore: :apple: app.py 호스트 변경, requirements.txt 추가

--- ML/main/app.py
+++ ML/main/app.py
@@ -66,4 +66,4 @@ class TokenGet(Resource):
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000)  # localhost:5000 접속 시 Swagger
+    app.run(host='0.0.0.0', port=5000)  # localhost:5000 접속 시 Swagger
