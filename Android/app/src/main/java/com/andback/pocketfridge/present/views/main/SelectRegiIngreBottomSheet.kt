b7c8032bb4a505af2ff806b75bf7228929b4d7fb
KimDoyeon <vlxjdus@gmail.com>
2022-05-06 15:52:17 +0900

    S06P31D206-201 [mo] feat : :sparkles: 식재료 등록 선택에 따라 화면이동
    nav graph에 action을 사용하지 않았습니다.

--- Android/app/src/main/java/com/andback/pocketfridge/present/views/main/SelectRegiIngreBottomSheet.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/SelectRegiIngreBottomSheet.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.andback.pocketfridge.R
 import com.andback.pocketfridge.databinding.FragmentSelectRegiIngreBinding
+import com.andback.pocketfridge.present.views.main.ingreupload.IngreUploadFragment
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class SelectRegiIngreBottomSheet : BottomSheetDialogFragment() {
@@ -19,6 +22,22 @@ class SelectRegiIngreBottomSheet : BottomSheetDialogFragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_select_regi_ingre, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.ivBottomSheetFClose.setOnClickListener {
+            dismiss()
+        }
+        binding.llBottomSheetFBarcode.setOnClickListener {
+            findNavController().navigate(R.id.barcodeScanFragment)
+            dismiss()
+        }
+        binding.llBottomSheetFUploadIngre.setOnClickListener {
+            findNavController().navigate(R.id.ingreUploadFragment)
+            dismiss()
+        }
     }
 
     companion object {
