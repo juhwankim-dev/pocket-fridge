a27035d8d23fd63b788f198e3c5fc60dc6c0048b
mdown <mdown@naver.com>
2022-05-06 10:03:23 +0900

    S06P31D206-28 [mo] feat: :sparkles: 마이페이지 UI 구현

---
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/mypage/MyPageFragment.kt
@@ -0,0 +1,9 @@
+package com.andback.pocketfridge.present.views.main.mypage
+
+import com.andback.pocketfridge.R
+import com.andback.pocketfridge.databinding.FragmentMyPageBinding
+import com.andback.pocketfridge.present.config.BaseFragment
+
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+
+}
