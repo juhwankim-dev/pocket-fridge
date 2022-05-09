14d82d7b914125d402a23033b0302a031af5d11f
김민수 <ms001118@naver.com>
2022-05-09 14:07:02 +0900

    S06P31D206-209 [mo] feat: :sparkles: 재료 유통기한 알림 매니저 생성

---
+++ Android/app/src/main/java/com/andback/pocketfridge/data/infra/IngreExpiryNotiManagerImpl.kt
@@ -0,0 +1,23 @@
+package com.andback.pocketfridge.data.infra
+
+import android.app.NotificationManager
+import android.content.Context
+import androidx.core.app.NotificationCompat
+import com.andback.pocketfridge.domain.infra.IngreExpiryNotiManager
+import com.andback.pocketfridge.present.config.INGRE_EXPIRY_NOTI_ID
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class IngreExpiryNotiManagerImpl @Inject constructor(
+    @ApplicationContext val context: Context
+): IngreExpiryNotiManager {
+    private val systemNotiManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    override fun sendNoti(title: String, body: String) {
+        val notiBuilder = NotificationCompat.Builder(context, INGRE_EXPIRY_NOTI_ID)
+        val noti = notiBuilder.setContentTitle(title)
+            .setContentText(body)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT).build()
+        systemNotiManager.notify(1, noti)
+    }
+}
