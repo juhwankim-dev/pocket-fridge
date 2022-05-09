841f38f3e2fc33f741badad0e5187d77e4a4ac0e
김민수 <ms001118@naver.com>
2022-05-09 14:04:29 +0900

    S06P31D206-209 [mo] feat: :sparkles: 알림 채널 상수 추가

--- Android/app/src/main/java/com/andback/pocketfridge/present/config/IOConsts.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/config/IOConsts.kt
@@ -3,3 +3,6 @@ package com.andback.pocketfridge.present.config
 const val DB_NAME = "app.db"
 const val INGRE_TABLE = "ingre_table"
 const val DATASTORE_NAME = "pocket_fridge"
+
+const val INGRE_EXPIRY_NOTI_ID = "ingre_noti_channel"
+const val INGRE_EXPIRY_NOTI_NAME = "식재료 유통기한 알림"
