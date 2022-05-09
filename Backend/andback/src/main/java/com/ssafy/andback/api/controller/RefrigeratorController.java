1f23f89ab2aced6659fbb0e7702c487ad5a3e2e0
rhksvlf0704 <rhksvlf0704@gmail.com>
2022-05-09 15:20:27 +0900

    S06P31D206-76 [be] feat: :sparkles: 냉장고 공유 그룹 생성 구현

--- Backend/andback/src/main/java/com/ssafy/andback/api/controller/RefrigeratorController.java
+++ Backend/andback/src/main/java/com/ssafy/andback/api/controller/RefrigeratorController.java
@@ -1,19 +1,24 @@
 package com.ssafy.andback.api.controller;
 
+import com.ssafy.andback.api.constant.ErrorCode;
 import com.ssafy.andback.api.dto.request.InsertRefrigeratorRequestDto;
 import com.ssafy.andback.api.dto.response.ListResponseDto;
 import com.ssafy.andback.api.dto.response.RefrigeratorResponseDto;
 import com.ssafy.andback.api.dto.response.BaseResponseDto;
+import com.ssafy.andback.api.exception.CustomException;
 import com.ssafy.andback.api.service.RefrigeratorService;
+import com.ssafy.andback.core.domain.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 
 import java.util.List;
@@ -57,6 +62,21 @@ public class RefrigeratorController {
         }
 
         return ResponseEntity.ok(BaseResponseDto.of(200, "success"));
+    }
+
+    @ApiOperation(value = "냉장고 그룹", notes = "냉장고 그룹을 생성한다.")
+    @PostMapping("/share/{refrigeratorId}")
+    public ResponseEntity<BaseResponseDto> createShareGroup(@ApiIgnore Authentication authentication, @PathVariable Long refrigeratorId) {
+
+        User user = (User) authentication.getPrincipal();
+
+        String result = refrigeratorService.createShareGroup(user, refrigeratorId);
+
+        if(result.equals("success")) {
+            return ResponseEntity.ok().body(BaseResponseDto.of(200, "냉장고 공유 그룹 생성 성공"));
+        }
+
+        throw new CustomException(ErrorCode.FAIL_SHARE_GROUP);
     }
 
 }
