e9db3ec652a4f8caaf913d7d7a020c2a564e96c1
hoonyCode <rladudgns456@naver.com>
2022-05-10 11:07:20 +0900

    S06P31D206-22 [be] feat: :sparkles: 소셜 로그인 구현 - RestTemplateConfig 생성 - application-local.yml 수정 - WebSecurityConfig 수정 - GoogleOauth 생성 - SocialOauth 생성 - OAuthService 소셜 로그인 서비스 구현 - SocialController 소셜 로그인 컨트롤러 구현 - SocialLoginType 소셜 로그인 타입 구현

---
+++ Backend/andback/src/main/java/com/ssafy/andback/config/auth/SocialOauth.java
@@ -0,0 +1,11 @@
+package com.ssafy.andback.config.auth;
+
+public interface SocialOauth {
+    /*
+    각 소설 로그인 페이지로 redirect할 URL Build
+    사용자로부터 로그인 요청을 받아 소셜 로그인 서버 인증용 코드 요청
+     */
+
+    String getOauthRedirectURL();
+
+}
