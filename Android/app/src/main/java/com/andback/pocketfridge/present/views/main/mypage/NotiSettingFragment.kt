02970a013e41516493eb6a12aa026351666c1269
김민수 <ms001118@naver.com>
2022-05-11 09:51:28 +0900

    S06P31D206-270 [mo] feat: :sparkles: NotiSettingFragment 생성

---
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/mypage/NotiSettingFragment.kt
@@ -0,0 +1,22 @@
+package com.andback.pocketfridge.present.views.main.mypage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.andback.pocketfridge.R
+import com.andback.pocketfridge.databinding.FragmentNotiSettingBinding
+import com.andback.pocketfridge.present.config.BaseFragment
+
+class NotiSettingFragment : BaseFragment<FragmentNotiSettingBinding>(R.layout.fragment_noti_setting) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+
+    }
+}
