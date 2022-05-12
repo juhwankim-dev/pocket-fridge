42cb421564b31f0311d53d7bc37232935e3a2f81
김민수 <ms001118@naver.com>
2022-05-12 21:47:53 +0900

    S06P31D206-271 [mo] feat: :sparkles: FcmToken 갱신 시 서버로 전송

--- Android/app/src/main/java/com/andback/pocketfridge/present/service/FcmService.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/service/FcmService.kt
@@ -1,7 +1,32 @@
 package com.andback.pocketfridge.present.service
 
+import android.annotation.SuppressLint
+import android.provider.Settings
+import com.andback.pocketfridge.data.model.FcmTokenEntity
+import com.andback.pocketfridge.domain.usecase.user.SendFcmTokenUseCase
 import com.google.firebase.messaging.FirebaseMessagingService
+import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
 
-class FcmService: FirebaseMessagingService() {
+@AndroidEntryPoint
+class FcmService @Inject constructor(
+    private val sendFcmTokenUseCase: SendFcmTokenUseCase
+): FirebaseMessagingService() {
+
+    @SuppressLint("HardwareIds")
+    override fun onNewToken(token:String) {
+        super.onNewToken(token)
+
+        val androidId: String = Settings.Secure.getString(
+            applicationContext.contentResolver,
+            Settings.Secure.ANDROID_ID
+        )
+
+        // 서버에 업데이트
+        sendFcmTokenUseCase(FcmTokenEntity(androidId, token)).subscribeOn(Schedulers.io())
+            .subscribe()
+    }
+
 
 }
