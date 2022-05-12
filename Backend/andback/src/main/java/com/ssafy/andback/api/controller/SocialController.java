699c8893d261e7aa3985c99178735f24bc18bb11
hoonyCode <rladudgns456@naver.com>
2022-05-12 16:10:59 +0900

    S06P31D206-267 [be] refact: :zap: 소셜 로그인시 API return 값 수정 - 소셜 로그인시 API return 값 기본 로그인과 동일하게 수정

--- Backend/andback/src/main/java/com/ssafy/andback/api/controller/SocialController.java
+++ Backend/andback/src/main/java/com/ssafy/andback/api/controller/SocialController.java
@@ -13,15 +13,14 @@ import org.springframework.web.servlet.view.RedirectView;
 
 
 /**
-*
-* SocialController
-* 소셜 로그인 컨트롤럴
-*
-* @author hoony
-* @version 1.0.0
-* 생성일 2022-05-10
-* 마지막 수정일 2022-05-10
-**/
+ * SocialController
+ * 소셜 로그인 컨트롤럴
+ *
+ * @author hoony
+ * @version 1.0.0
+ * 생성일 2022-05-10
+ * 마지막 수정일 2022-05-10
+ **/
 
 @Api(tags = {"08. 소셜 로그인 API"})
 @Controller
@@ -63,7 +62,7 @@ public class SocialController {
             return ResponseEntity.ok(new SingleResponseDto<String>(200, "fail", "소셜 로그인에 실패했습니다"));
         }
 
-        return ResponseEntity.ok(new SingleResponseDto<String>(200, "success", result));
+        return ResponseEntity.ok(new SingleResponseDto<>(200, "로그인 성공", result));
     }
 
 }
