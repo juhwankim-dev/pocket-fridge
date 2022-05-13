47729bf1c9d1d483594a13768ee8e7eb8424870f
김민수 <ms001118@naver.com>
2022-05-13 14:55:33 +0900

    S06P31D206-274 [mo] feat: :sparkles: 푸시 알림 탭 LoginActivity로 이동 추가

--- Android/app/src/main/java/com/andback/pocketfridge/data/infra/IngreExpiryNotiManagerImpl.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/data/infra/IngreExpiryNotiManagerImpl.kt
@@ -1,11 +1,14 @@
 package com.andback.pocketfridge.data.infra
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 import com.andback.pocketfridge.R
 import com.andback.pocketfridge.domain.infra.IngreExpiryNotiManager
 import com.andback.pocketfridge.present.config.INGRE_EXPIRY_NOTI_ID
+import com.andback.pocketfridge.present.views.user.UserActivity
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
@@ -13,13 +16,23 @@ class IngreExpiryNotiManagerImpl @Inject constructor(
     @ApplicationContext val context: Context
 ): IngreExpiryNotiManager {
     private val systemNotiManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    val notifyIntent = Intent(context, UserActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    }
+    val notifyPendingIntent = PendingIntent.getActivity(
+        context, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT
+    )
 
     override fun sendNoti(title: String, body: String) {
         val notiBuilder = NotificationCompat.Builder(context, INGRE_EXPIRY_NOTI_ID)
+
         val noti = notiBuilder.setContentTitle(title)
             .setContentText(body)
             .setSmallIcon(R.mipmap.ic_launcher_foreground)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT).build()
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(notifyPendingIntent)
+            .build()
+
         systemNotiManager.notify(1, noti)
     }
 }
