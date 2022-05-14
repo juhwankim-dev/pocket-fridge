6f7b4fcc645497a0505c8ee8e1e052571afe1ea9
김민수 <ms001118@naver.com>
2022-05-14 16:46:06 +0900

    S06P31D206-279 [mo] feat: :sparkles: 식재료 등록 bottom nav 클릭 시 자신 소유의 냉장고가 없으면 냉장고 관리 페이지로 이동

--- Android/app/src/main/java/com/andback/pocketfridge/present/views/main/MainActivity.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/MainActivity.kt
@@ -4,7 +4,9 @@ package com.andback.pocketfridge.present.views.main
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.activity.viewModels
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI.onNavDestinationSelected
@@ -12,6 +14,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.andback.pocketfridge.R
 import com.andback.pocketfridge.databinding.ActivityMainBinding
 import com.andback.pocketfridge.present.config.BaseActivity
+import com.andback.pocketfridge.present.views.main.fridge.FridgeViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,6 +22,8 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private val TAG = "MainActivity_debuk"
+    private val fridgeViewModel: FridgeViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setBottomNav()
@@ -51,6 +56,13 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
         binding.bnvMain.setOnItemSelectedListener { menuItem ->
             if (menuItem.itemId == R.id.selectRegiIngre) {
+                // 자신 소유의 냉장고가 하나라도 없으면 냉장고 관리 화면으로 이동
+                if(!hasOwnFridge()) {
+                    moveToFridgeManageFragment(navController)
+                    showToastMessage("식재료를 등록하려면 자신 소유의 냉장고를 추가해 주세요.")
+                    return@setOnItemSelectedListener false
+                }
+
                 val modalBottomSheet = SelectRegiIngreBottomSheet()
                 modalBottomSheet.show(supportFragmentManager, SelectRegiIngreBottomSheet.TAG)
 
@@ -59,5 +71,13 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
             onNavDestinationSelected(menuItem, navController)
             true
         }
+    }
+
+    private fun hasOwnFridge(): Boolean {
+        return fridgeViewModel.fridges.value != null && fridgeViewModel.fridges.value!!.any { it.isOwner }
+    }
+
+    private fun moveToFridgeManageFragment(navController: NavController) {
+        navController.navigate(R.id.fridgeManageFragment)
     }
 }
