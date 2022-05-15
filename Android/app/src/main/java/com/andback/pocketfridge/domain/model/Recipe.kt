cbba83608fbaa517085ea358e37d68c97c12f080
mdown <mdown@naver.com>
2022-05-15 21:18:40 +0900

    S06P31D206-75 [mo] feat: :sparkles: dto와 entity를 구분하기 위해 Recipe 추가 생성

---
+++ Android/app/src/main/java/com/andback/pocketfridge/domain/model/Recipe.kt
@@ -0,0 +1,19 @@
+package com.andback.pocketfridge.domain.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Recipe(
+    @SerializedName("recipeId") val id: Int,
+    @SerializedName("recipeAllIngredient") val allIngredients: String,
+    @SerializedName("recipeContent") val content: String,
+    @SerializedName("recipeFoodName") val foodName: String,
+    @SerializedName("recipeImage") val url: String?,
+    @SerializedName("recipeServing") val serving: String,
+    @SerializedName("recipeTime") val time: String,
+    @SerializedName("recipeType") val type: String,
+    @SerializedName("recipeLike") var like: Boolean,
+    var isRecommendation: Boolean
+) : Parcelable
