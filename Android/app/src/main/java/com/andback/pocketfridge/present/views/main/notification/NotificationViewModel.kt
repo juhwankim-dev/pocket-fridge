0b952926782cd8ecf1448712ded6e8863d0b8975
김민수 <ms001118@naver.com>
2022-05-16 09:03:46 +0900

    S06P31D206-220 [mo] feat: :sparkles: NotificationViewModel 알림 목록 받아서 라이브데이터에 반영

---
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/notification/NotificationViewModel.kt
@@ -0,0 +1,44 @@
+package com.andback.pocketfridge.present.views.main.notification
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.andback.pocketfridge.data.model.NotificationEntity
+import com.andback.pocketfridge.domain.repository.NotificationRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
+
+@HiltViewModel
+class NotificationViewModel @Inject constructor(
+    private val repository: NotificationRepository
+): ViewModel() {
+    private val compositeDisposable = CompositeDisposable()
+    private val _notificationList = MutableLiveData<List<NotificationEntity>>()
+    val notificationList: LiveData<List<NotificationEntity>> get() = _notificationList
+
+    init {
+        getNotifications()
+    }
+
+    fun getNotifications() {
+        val single = repository.getNotifications().subscribeOn(Schedulers.io())
+            .subscribe(
+                { response ->
+                    response.data?.let {
+                        _notificationList.postValue(it)
+                    }
+                },
+                {
+                    // TODO: 알림 받기 실패 처리
+                }
+            )
+        compositeDisposable.add(single)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
+}
