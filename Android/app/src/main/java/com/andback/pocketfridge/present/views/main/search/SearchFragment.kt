c5c2ad79f221a3af61cb4f5088e276455166f8c3
mdown <mdown@naver.com>
2022-05-19 00:47:33 +0900

    S06P31D206-319 [mo] feat: :sparkles: 모달을 둥글게, 리스트를 리사이클러뷰로 변경

--- Android/app/src/main/java/com/andback/pocketfridge/present/views/main/search/SearchFragment.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/search/SearchFragment.kt
@@ -6,10 +6,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
-import android.widget.ArrayAdapter
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.andback.pocketfridge.R
@@ -17,6 +17,7 @@ import com.andback.pocketfridge.databinding.FragmentSearchBinding
 import com.andback.pocketfridge.databinding.FragmentSortListBinding
 import com.andback.pocketfridge.domain.model.Ingredient
 import com.andback.pocketfridge.present.config.BaseFragment
+import com.andback.pocketfridge.present.views.main.recipe.RecipeItemDecoration
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,7 +26,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
     private val rvAdapter = SearchAdapter()
     private val viewModel: SearchViewModel by viewModels()
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -89,7 +89,7 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_sea
 
     private fun setSort() {
         val list = resources.getStringArray(R.array.spinner_list)
-        val sortAdapter = ArrayAdapter(requireContext(), R.layout.item_dropdown_list, list)
+        val searchModalAdapter = SearchModalAdapter(list)
 
         binding.tvSearchFSort.text = list[SearchAdapter.SORT_BY_EXP]
 
@@ -102,13 +102,18 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_sea
                 dialogBinding.ibSortListFClose.setOnClickListener {
                     dismiss()
                 }
-                dialogBinding.lvSsortListF.apply {
-                    adapter = sortAdapter
-                    setOnItemClickListener { adapterView, view, position, id ->
-                        rvAdapter.sortList(position)
-                        binding.tvSearchFSort.text = list[position]
-                        dismiss()
-                    }
+                dialogBinding.rvSortListF.apply {
+                    layoutManager = LinearLayoutManager(context)
+                    adapter = searchModalAdapter
+                    addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
+
+                    searchModalAdapter.setItemClickListener(object : SearchModalAdapter.ItemClickListener{
+                        override fun onClick(position: Int) {
+                            rvAdapter.sortList(position)
+                            binding.tvSearchFSort.text = list[position]
+                            dismiss()
+                        }
+                    })
                 }
             }.show()
         }
