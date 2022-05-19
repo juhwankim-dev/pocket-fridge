78007d764f8689cad3d3d7c6c63c370cb12dc40f
KimDoyeon <vlxjdus@gmail.com>
2022-05-19 13:06:32 +0900

    [mo] style : :art: 안 쓰는 코드 지우기

--- Android/app/src/main/java/com/andback/pocketfridge/present/views/main/barcode/BarcodeScanFragment.kt
+++ Android/app/src/main/java/com/andback/pocketfridge/present/views/main/barcode/BarcodeScanFragment.kt
@@ -7,8 +7,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.EditText
-import android.widget.TextView
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
